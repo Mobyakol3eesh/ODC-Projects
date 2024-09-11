7ecce7859d3b070877d493173d4559330a4f3468
mobyakol3eesh <mohamed_hesham203@icloud.com>
2024-09-12 02:54:27 +0300

    initail right commit

--- Bistro-Bliss/src/app/app.component.ts
+++ Bistro-Bliss/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { HeroComponent } from './hero/hero.component';
+import { BrowseMenuComponent } from './browse-menu/browse-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent,TopHeaderComponent],
+  imports: [RouterOutlet,NavComponent,TopHeaderComponent,HeroComponent,BrowseMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Bistro-Bliss/src/app/browse-menu/browse-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './browse-menu.component.html',
+  styleUrl: './browse-menu.component.scss'
+})
+export class BrowseMenuComponent {
+
+}
