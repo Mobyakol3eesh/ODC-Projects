5f54fa2bdcd5fce635c308647ac8c176790c11b1
mobyakol3eesh <mohamed_hesham203@icloud.com>
2024-09-13 14:55:07 +0300

    finshed half of contact us

--- Bistro-Bliss/src/app/switch-page/switch-page.component.ts
+++ Bistro-Bliss/src/app/switch-page/switch-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-switch-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './switch-page.component.html',
   styleUrl: './switch-page.component.scss'
 })
